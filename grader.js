d5d5870f489181280a9734054cde4827eee12517
Ubuntu <ubuntu@ip-10-255-26-119.us-west-2.compute.internal>
2013-07-07 16:05:10 +0000

    HW 3

--- grader.js
+++ grader.js
@@ -24,8 +24,10 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://www.stackoverflow.com";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -44,15 +46,37 @@ var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
-    var checks = loadChecks(checksfile).sort();
-    var out = {};
-    for(var ii in checks) {
-        var present = $(checks[ii]).length > 0;
-        out[checks[ii]] = present;
-    }
-    return out;
+var checkStr = function(str, checksfile) {
+  $ = str;
+  var checks = loadChecks(checksfile).sort();
+  var out = {};
+  for(var ii in checks) {
+    var present = $(checks[ii]).length > 0;
+    out[checks[ii]] = present;
+  }
+  return out;
+};
+
+var checkHtmlFile = function(htmlfile, checksfile, onComplete) {
+    str = cheerioHtmlFile(htmlfile);
+    onComplete(checkStr(str, checksfile));
+};
+
+
+var checkUrl = function(url, checksfile, onComplete) {
+    rest.get(url).on('complete', function(result, response) {
+        if (result instanceof Error) {
+            console.error('Error: ' + result.message);
+            return;
+        }
+
+        if (response.statusCode != 200) {
+            console.error('Warning! Unexpected status code ' + response.statusCode);
+        }
+        
+        str = cheerio.load(result);
+        onComplete(checkStr(str, checksfile));
+    });
 };
 
 var clone = function(fn) {
@@ -61,14 +85,29 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var printCheckResult = function(checkJson) {
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+};
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists))
+        .option('-u, --url <url>', 'URL to check')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    
+    if (program.url && program.file) {
+        console.error('Usage error: You may either specify a file or a URL');
+        program.help();
+    } else if (program.url) {
+        checkUrl(program.url, program.checks, printCheckResult);
+    } else {
+        if (!program.file) {
+            program.file = HTMLFILE_DEFAULT;
+        }
+        checkHtmlFile(program.file, program.checks, printCheckResult);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
