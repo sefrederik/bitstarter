40d91b5d81cceb89ca3d587865531180233799ce
Frederik Hermans <frederik.ac.hermans+se@gmail.com>
2013-07-23 20:54:23 +0200

    Serve simple.html and local content

--- web.js
+++ web.js
@@ -4,9 +4,11 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  buf = fs.readFileSync('index.html');
+  buf = fs.readFileSync('simple.html');
   response.send(buf.toString());
 });
+
+app.use('/static', express.static(__dirname + '/static'));
 
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
